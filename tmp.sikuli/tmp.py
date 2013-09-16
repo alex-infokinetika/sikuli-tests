3862816c81f02bc4cb41848a2503f9dc4cff2a04
alex-infokinetika <alex@infokinetika.ru>
2013-09-16 17:53:00 +0400

    доделал tmp
    довёл функцию проверки дерева объектов до некоторого готового состояния

--- tmp.sikuli/tmp.py
+++ tmp.sikuli/tmp.py
@@ -60,22 +60,54 @@ def tmp():
     closeCurTab()
 #--------------заготовки:
     newMapTab()
-
     doubleClick(Pattern("ncrpuh.png").similar(0.80).targetOffset(2,1))
-    wait(Pattern("1379080119801.png").similar(0.80))
+    try:
+        wait(Pattern("1379080119801.png").similar(0.80))
+        print(u"Переместились к объекту по даблклику")
+    except:
+        print(u"Не переместились к объекту по даблклику")
+        exit(3)
     click(Pattern("Eucmwi.png").similar(0.80).targetOffset(-72,0))
-    waitVanish("1379080119801.png")
-    wait("1379080342892.png")
-    wait("EcsmvVar10QO.png")
+    try:
+        waitVanish("1379080119801.png", 5)
+        wait("1379080342892.png")
+        print(u"Скрыли объект")
+        wait("EcsmvVar10QO.png")
+        print(u"И галочка в дереве исчезла")
+    except:
+        print(u"Не скрылся объект или галочка в дереве не исчезла")
+        exit(4)
     click(Pattern("Eucmwi.png").similar(0.80).targetOffset(-72,0))
-    wait(Pattern("1379080119801.png").similar(0.80))
+    try:
+        wait(Pattern("1379080119801.png").similar(0.80),5)
+        print(u"Вернули видимость объекта")
+    except:
+        print(u"Видимость объекта не восстановилась")
+        exit(5)
     click(Pattern("OncxmMoumopm.png").similar(0.80).targetOffset(-55,31))
-    wait(Pattern("IIO61eVluani.png").similar(0.90))
-    waitVanish("1379080119801.png")
+    try:
+        wait(Pattern("IIO61eVluani.png").similar(0.90))
+        print(u"Сняли галочки со всего дерева объектов")
+    except:
+        print(u"Галочки, со всего дерева объектов, не снялись")
+        exit(6)
+    try:
+        waitVanish("1379080119801.png",30)# Уменьшить время ожидания после починки бага http://idea.navstat.ru/tickets/7971
+        wait("1379080342892.png")
+        print(u"Объект сурылся")
+    except:
+        print(u"Не скрылся объект")
+        exit(7)
     click(Pattern("OncxmMoumopm.png").similar(0.80).targetOffset(-55,31))
-    wait(Pattern("1379080789295.png").similar(0.90))
-    wait(Pattern("1379080119801.png").similar(0.80)) # Найден баг, не сразу исчезает и появляется машинка при установке снятии галок со всех объектов, уточнить причины задержки
-
+    try:
+        wait(Pattern("1379080789295.png").similar(0.90))
+        print(u"Вернули галочки видимости всем объектам")
+        waitVanish("1379080342892.png",30)
+        wait(Pattern("1379080119801.png").similar(0.80)) # Найден баг, не сразу исчезает и появляется машинка при установке снятии галок со всех объектов, уточнить причины задержки
+        print(u"Вернулась видимость объекта")
+    except:
+        print(u"Видимость объекта не восстановилась")
+        exit(8)
 #-----------------------
     type(Key.F4, KeyModifier.ALT)
 
