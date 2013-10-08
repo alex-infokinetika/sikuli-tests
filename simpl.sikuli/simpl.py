d5e93801105da6fb876ea9d3b462b4fc6500893b
alex-infokinetika <alex@infokinetika.ru>
2013-10-08 17:52:05 +0400

    commit

--- simpl.sikuli/simpl.py
+++ simpl.sikuli/simpl.py
@@ -1,10 +1,49 @@
 from sikuli import *
 # -*- coding: utf-8 -*-
 import os
+import time
+import datetime
 
-
-myPath = os.environ.get("GIT_HOME") + u"sikuli-tests"
+myPath = os.path.join(os.environ.get("GIT_HOME"), u"sikuli-tests")
 if not myPath in sys.path:
 	sys.path.append(myPath)
 # Импорт ------------------------------
 import baseFunction as BF
+
+def fff():
+	find("2.png")
+
+
+
+def reportTest1(reportName):
+	start = time.time()
+	baseDir = os.path.join(os.environ.get("GIT_HOME"), u"sikuli-tests", u"img", u"report_1")
+	fList = os.listdir(os.path.join(baseDir,reportName));
+	patternList = []
+	for f in fList:
+		patternList.append(os.path.join(baseDir,reportName,f))
+	print patternList
+	BF.clearData()
+#	keyer.editKeyAndService("404C2A00-B173-4844-BA59-9A6F296479E7", "http://services.navstat.infokinetika.net")
+	BF.firstStartNavstat()
+# Переходим на таб отчётов (просто закрыв таб с картой)
+	BF.closeCurTab()
+	click(Pattern("6917gag.png").similar(0.80).targetOffset(-1,0)) # Заглушка http://idea.navstat.ru/tickets/6917
+	type(Key.TAB) #затычка чтобы снять фокус с отчёта Итоги по автопарку, т.е. сделать его похожим на остальные
+	click(patternList[0])
+	click("runReport.png")
+	try:
+		find("question.png")
+		click("da.png")
+	except:
+		print (u".")
+	try:
+		reportStart = time.time()
+		BF.waitAll(patternList,2000)
+		print (u"Отчёт выполнен за "), datetime.timedelta(seconds=time.time()-reportStart)
+	except:
+		print (u"Отчёт не выполнен или результат не корректен")
+		type(Key.F4, KeyModifier.ALT)
+		exit(0)
+	print (u"Время выполнения теста: "), datetime.timedelta(seconds=time.time()-start)
+#	type(Key.F4, KeyModifier.ALT)
